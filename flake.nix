d611822e1b9ecf656914ed009c01992108175582
Jonathan Shen <jonathan@mlabs.com>
2024-01-15 17:41:24 -0500

    update kupo to 2.7.2

--- flake.nix
+++ flake.nix
@@ -1,35 +1,38 @@
 {
   description = "NixOS module for Kupo";
-  nixConfig = {
-    extra-experimental-features = [ "nix-command" "flakes" ];
-    allow-import-from-derivation = "true";
-    cores = "1";
-    max-jobs = "auto";
-    auto-optimise-store = "true";
-  };
+  #nixConfig = {
+  #  extra-experimental-features = [ "nix-command" "flakes" ];
+  #  allow-import-from-derivation = "true";
+  #  cores = "1";
+  #  max-jobs = "auto";
+  #  auto-optimise-store = "true";
+  #};
   inputs = {
     # should follow inputs in https://github.com/CardanoSolutions/kupo/blob/master/default.nix#L22
     # haskell-nix.url = github:input-output-hk/haskell.nix/974a61451bb1d41b32090eb51efd7ada026d16d9;
-    haskell-nix.url = github:input-output-hk/haskell.nix/b90fbaa272a6d17ddc21164ca3056e1618feafcd;
-    iohk-nix.url = github:input-output-hk/iohk-nix/edb2d2df2ebe42bbdf03a0711115cf6213c9d366;
+    haskell-nix.url = github:input-output-hk/haskell.nix;
+    iohk-nix.url = github:input-output-hk/iohk-nix;
 
     nixpkgs.follows = "haskell-nix/nixpkgs";
-    iohk-nix.inputs.nixpkgs.follows = "haskell-nix/nixpkgs";
+    # iohk-nix.inputs.nixpkgs.follows = "haskell-nix/nixpkgs";
     kupo = {
-      url = github:CardanoSolutions/kupo;
+      # 2.7.2
+      url = "git+file:///home/jon/projects/kupo?ref=release/v2.7";
+      # url = github:CardanoSolutions/kupo;
       flake = false;
     };
     chap = {
-      url = "github:input-output-hk/cardano-haskell-packages/316e0a626fed1a928e659c7fc2577c7773770f7f";
+      url = "github:intersectmbo/cardano-haskell-packages?ref=repo";
       flake = false;
     };
   };
-  outputs = inputs@{ self, nixpkgs, haskell-nix, chap, ... }:
+  outputs = inputs@{ self, nixpkgs, haskell-nix, iohk-nix, chap, ... }:
     let
-      perSystem = nixpkgs.lib.genAttrs [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-      pkgs = perSystem (system: import nixpkgs { inherit system; overlays = [ haskell-nix.overlay ]; inherit (haskell-nix) config; });
+      perSystem = nixpkgs.lib.genAttrs [ "x86_64-linux" ];
+      pkgs = perSystem (system: import nixpkgs { inherit system; overlays =
+        [haskell-nix.overlay iohk-nix.overlays.crypto iohk-nix.overlays.haskell-nix-crypto]; inherit (haskell-nix) config; });
       project = perSystem (system: pkgs.${system}.haskell-nix.project {
-        compiler-nix-name = "ghc8107";
+        compiler-nix-name = "ghc963";
         projectFileName = "cabal.project";
         src = nixpkgs.lib.cleanSourceWith {
           name = "kupo-src";
